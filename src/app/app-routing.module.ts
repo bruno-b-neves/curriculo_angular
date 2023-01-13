e95ee79bf93b127e5436afd46f23a7d11b1dc7ee
bruno-b-neves <brunobningles@gmail.com>
2023-01-13 16:14:34 -0300

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Error404Component } from './site-principal/error404/error404.component';
+import { HomeComponent } from './site-principal/site-principal.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: '**', component: Error404Component}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './site-principal/site-principal.component';
+import { NavbarComponent } from './site-principal/navbar/navbar.component';
+import { Error404Component } from './site-principal/error404/error404.component';
+import { HeaderComponent } from './site-principal/header/header.component';
+import { ContentOneComponent } from './site-principal/content-one/content-one.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    Error404Component,
+    HeaderComponent,
+    ContentOneComponent,
   ],
   imports: [
     BrowserModule,
